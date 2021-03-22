4796a4a19de65a1b780a64d3e4b46d514ec8c65f
Bill Liu Macbook <liuyuchen1997@hotmail.com>
2021-03-21 20:47:29 -0700

    Adjusted initial zoom viewpoint

--- js/bubbleChart.js
+++ js/bubbleChart.js
@@ -183,7 +183,8 @@ class BubbleChart {
     initialZoom() {
         let vis = this;
         // initial zoom
-        let initialTransform = d3.zoomIdentity.scale(5).translate(0,-150);
+        let initialTransform = d3.zoomIdentity.translate(vis.innerWidth/2, vis.innerHeight/2)
+            .scale(5).translate(-vis.innerWidth/5,-vis.innerHeight/1.3);
         vis.svg.call(vis.zoom.transform, initialTransform);
         // go back to normal
         vis.svg
