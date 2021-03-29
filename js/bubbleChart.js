39d41a5b23c739961dd2af2f7b1052188d6c02a9
thcui <albhang@126.com>
2021-03-29 15:38:20 -0700

    partial refactorization done for the line chart

--- js/bubbleChart.js
+++ js/bubbleChart.js
@@ -9,21 +9,22 @@ class BubbleChart {
         this.data = _data
         this.initVis();
     }
-    initVis(){
+
+    initVis() {
         let vis = this;
 
         vis.initFlag = true;
-        vis.title_height=30
+        vis.title_height = 30
 
-        vis.custom_container_y=70
-        vis.custom_container_width=300
+        vis.custom_container_y = 70
+        vis.custom_container_width = 300
 
 
         // Calculate inner chart size. Margin specifies the space around the actual chart.
-        vis.innerWidth = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right-vis.custom_container_width;
+        vis.innerWidth = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right - vis.custom_container_width;
         vis.innerHeight = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
-        vis.custom_container_height=vis.innerHeight/2-10
-        vis.custom_container_x=vis.innerWidth+80
+        vis.custom_container_height = vis.innerHeight / 2 - 10
+        vis.custom_container_x = vis.innerWidth + 80
         // Define size of SVG drawing area
         vis.svg = d3
             .select(vis.config.parentElement)
@@ -31,20 +32,20 @@ class BubbleChart {
             .attr("height", vis.config.containerHeight);
 
         vis.svg.append('text')
-            .attr('id','bubblechart_title')
+            .attr('id', 'bubblechart_title')
 
-        vis.custom_container=vis.svg.append('g').attr('id','custom_container')
+        vis.custom_container = vis.svg.append('g').attr('id', 'custom_container')
             .style("font-size", "15px")
             .attr("font-weight", "700")
-            .attr('fill','#dddddd')
+            .attr('fill', '#dddddd')
         vis.custom_container.append('text')
-            .attr("transform", `translate(${vis.custom_container_x},${vis.custom_container_y-40})`)
+            .attr("transform", `translate(${vis.custom_container_x},${vis.custom_container_y - 40})`)
             .text('Customize your own basket of')
         vis.custom_container.append('text')
-            .attr("transform", `translate(${vis.custom_container_x},${vis.custom_container_y-20})`)
+            .attr("transform", `translate(${vis.custom_container_x},${vis.custom_container_y - 20})`)
             .text(' stocks by dragging the bubble here! ')
 
-        vis.custom_basket=vis.custom_container.append('rect')
+        vis.custom_basket = vis.custom_container.append('rect')
             .attr("transform", `translate(${vis.custom_container_x},${vis.custom_container_y})`)
             .attr('width', vis.custom_container_width)
             .attr('height', vis.custom_container_height)
@@ -52,19 +53,19 @@ class BubbleChart {
             .attr("rx", 10)
             .attr("ry", 10)
             .attr("fill-opacity", '0.5')
-        vis.custom_container.append('text') .attr("transform", `translate(${vis.custom_container_x},${vis.custom_container_y+15})`)
+        vis.custom_container.append('text').attr("transform", `translate(${vis.custom_container_x},${vis.custom_container_y + 15})`)
             .text('Basket1')
 
-        vis.custom_basket2=vis.custom_basket.clone()
-            .attr("transform", `translate(${vis.custom_container_x},${vis.custom_container_y+vis.custom_container_height+10})`)
+        vis.custom_basket2 = vis.custom_basket.clone()
+            .attr("transform", `translate(${vis.custom_container_x},${vis.custom_container_y + vis.custom_container_height + 10})`)
             .attr("fill", 'yellow')
             .attr("fill-opacity", '0.5')
-        vis.custom_container.append('text') .attr("transform", `translate(${vis.custom_container_x},${vis.custom_container_y+vis.custom_container_height+25})`)
+        vis.custom_container.append('text').attr("transform", `translate(${vis.custom_container_x},${vis.custom_container_y + vis.custom_container_height + 25})`)
             .text('Basket2')
 
 
-        vis.custom_selection=[]
-        vis.clones={}
+        vis.custom_selection = []
+        vis.clones = {}
 
         // Append group element that will contain our actual chart
         // and position it according to the given margin config
@@ -90,10 +91,10 @@ class BubbleChart {
         vis.chart = vis.chartArea.append('g')
             .attr('clip-path', 'url(#chart-mask)');
 
-        vis.xScale = d3.scaleLinear().range([10, vis.innerWidth-55]);
-        vis.yScale = d3.scaleLinear().range([vis.innerHeight-55, 55]);
+        vis.xScale = d3.scaleLinear().range([10, vis.innerWidth - 55]);
+        vis.yScale = d3.scaleLinear().range([vis.innerHeight - 55, 55]);
         vis.radiusScale = d3.scaleSqrt().range([5, 50]);
-        vis.unzommed_radiusScale= d3.scaleSqrt().range([5, 50]);
+        vis.unzommed_radiusScale = d3.scaleSqrt().range([5, 50]);
         vis.Yaxis = d3.axisLeft(vis.yScale)
             .tickSize(-vis.innerWidth)
             .tickPadding(10)
@@ -103,7 +104,7 @@ class BubbleChart {
             .tickSize(-vis.innerHeight)
             .tickPadding(10)
             .ticks(10)
-            .tickFormat(d => d/10**9 +'B');
+            .tickFormat(d => d / 10 ** 9 + 'B');
 
         // Append axis titles
         vis.chartArea.append('text')
@@ -123,13 +124,13 @@ class BubbleChart {
         // zoom and pan
         vis.zoom = d3.zoom()
             .scaleExtent([1, 40])
-            .translateExtent([[-100,-100],[vis.config.containerWidth+100,vis.config.containerHeight+100]])
+            .translateExtent([[-100, -100], [vis.config.containerWidth + 100, vis.config.containerHeight + 100]])
             .on("zoom", function (event) {
                 vis.zoomed(event);
             });
         vis.autoZoom = d3.zoom()
             .scaleExtent([1, 40])
-            .translateExtent([[-100,-100],[vis.config.containerWidth+100,vis.config.containerHeight+100]])
+            .translateExtent([[-100, -100], [vis.config.containerWidth + 100, vis.config.containerHeight + 100]])
             .on("zoom", function (event) {
                 vis.autoZoomed(event);
             });
@@ -138,7 +139,7 @@ class BubbleChart {
 
     }
 
-    updateVis(){
+    updateVis() {
         let vis = this;
 
         vis.resetZoom();
@@ -156,7 +157,7 @@ class BubbleChart {
         vis.renderVis();
     }
 
-    renderVis(){
+    renderVis() {
         let vis = this;
         let original;
         let clone;
@@ -195,40 +196,37 @@ class BubbleChart {
                     .remove()
             );
         vis.circle
-            .on("mouseover",this.showToolTip)
-            .on("mouseout",this.hideToolTip)
-            .on('click',this.clickBubble )
+            .on("mouseover", this.showToolTip)
+            .on("mouseout", this.hideToolTip)
+            .on('click', this.clickBubble)
             .call(
                 d3.drag()
-                    .on("start", function (event,d){
-                        original=d3.select(this)
-                        clone=vis.svg.append('circle')
-                            .attr("fill",  colorScheme(d.industry))
+                    .on("start", function (event, d) {
+                        original = d3.select(this)
+                        clone = original.clone()
                             .attr("r", vis.unzommed_radiusScale(d.marketcap))
-                            .on("mouseover",vis.showToolTip)
-                            .on("mouseout",vis.hideToolTip)
-                            .attr('cx',-100)
-                            .attr('cy',-100)
-                            .attr("opacity", 0.7)
-                        clone.attr('clip-path',"polygon(21% 51%, 41% 78%, 78% 26%, 89% 38%, 42% 97%, 11% 62%)")
+                            .attr('cx', -100)
+                            .attr('cy', -100)
+                            .attr('transform', null)
+                        clone.attr('clip-path', "polygon(21% 51%, 41% 78%, 78% 26%, 89% 38%, 42% 97%, 11% 62%)")
 
-                        clone.each(function() { vis.custom_container.append(() => this); });
+                        clone.each(function () {
+                            vis.custom_container.append(() => this);
+                        });
 
                     })
-                    .on("drag", function (event,d){
-
-                        clone.attr("cx",  event.x).attr("cy",  event.y)
-                            .attr('class','custom')
-                            .on("mouseover",vis.showToolTip)
-                            .on("mouseout",vis.hideToolTip)
+                    .on("drag", function (event, d) {
+                        clone.attr("cx", event.x).attr("cy", event.y)
+                            .attr('class', 'custom')
                     })
                     .on("end", dragend)
             );
 
-        function dragend(event,d){
-            let basket_index=0
+        function dragend(event, d) {
+            let basket_index = 0
             let text
             let data
+
             function remove_one_item(arr, value) {
                 let index = arr.indexOf(value);
                 if (index > -1) {
@@ -238,56 +236,59 @@ class BubbleChart {
             }
 
             if (event.x >= vis.custom_container_x && event.y >= vis.custom_container_y) {
-                text=vis.custom_container.append('text').text(d.symbol).attr("transform",`translate(${event.x},${event.y+10})`).attr('color','#000000').attr('font-size','20')
-                if(event.y <= vis.custom_container_y+vis.custom_container_height){
-                    data=custom_data
-                    basket_index=1
+                text = vis.custom_container.append('text').text(d.symbol).attr("transform", `translate(${event.x},${event.y + 10})`).attr('color', '#000000').attr('font-size', '20')
+                if (event.y <= vis.custom_container_y + vis.custom_container_height) {
+                    data = custom_data
+                    basket_index = 1
                     selected_stock_symbol.push('Basket')
+                } else {
+                    basket_index = 2
+                    data = custom_data2
+                    selected_stock_symbol.push('Basket2')
                 }
-                else{
-                    basket_index=2
-                    data=custom_data2
-                    selected_stock_symbol.push('Basket2')}
                 data.push(d.symbol)
 
-                clone.call( d3.drag().on("drag", function (event,d){
-                    d3.select(this).attr("cx", event.x).attr("cy",  event.y)
-                }). on("end", function (event,d){
-                    if (d3.select(this).attr("cx")>= vis.custom_container_x && d3.select(this).attr("cy")>= vis.custom_container_y){
-                        if(event.y <= vis.custom_container_y+vis.custom_container_height&& basket_index===2){
-                            basket_index=1
-                            remove_one_item(custom_data2,d.symbol)
+                clone.call(d3.drag().on("drag", function (event, d) {
+                    d3.select(this).attr("cx", event.x).attr("cy", event.y)
+                }).on("end", function (event, d) {
+                    if (d3.select(this).attr("cx") >= vis.custom_container_x && d3.select(this).attr("cy") >= vis.custom_container_y) {
+                        if (event.y <= vis.custom_container_y + vis.custom_container_height && basket_index === 2) {
+                            basket_index = 1
+                            remove_one_item(custom_data2, d.symbol)
                             custom_data.push(d.symbol)
                             selected_stock_symbol.push('Basket')
                         }
-                        if(event.y > vis.custom_container_y+vis.custom_container_height&& basket_index===1){
-                            basket_index=2
-                            remove_one_item(custom_data,d.symbol)
+                        if (event.y > vis.custom_container_y + vis.custom_container_height && basket_index === 1) {
+                            basket_index = 2
+                            remove_one_item(custom_data, d.symbol)
                             custom_data2.push(d.symbol)
                             selected_stock_symbol.push('Basket2')
                         }
 
                         text.remove()
-                        text=vis.custom_container.append('text').text(d.symbol).attr("transform",`translate(${event.x},${event.y+10})`).attr('color','#000000').attr('font-size','20')
+                        text = vis.custom_container.append('text').text(d.symbol).attr("transform", `translate(${event.x},${event.y + 10})`).attr('color', '#000000').attr('font-size', '20')
 
-                    }
-                    else{
-                        if(basket_index===1){
-                            remove_one_item(custom_data,d.symbol)
+                    } else {
+                        if (basket_index === 1) {
+                            remove_one_item(custom_data, d.symbol)
                         }
-                        if(basket_index===2){
-                            remove_one_item(custom_data2,d.symbol)
+                        if (basket_index === 2) {
+                            remove_one_item(custom_data2, d.symbol)
                         }
 
                         d3.select(this).remove()
                         text.remove()
 
                     }
-                    if(custom_data.length===0){
-                        selected_stock_symbol=selected_stock_symbol.filter(v=>{return v!=='Basket'})
+                    if (custom_data.length === 0) {
+                        selected_stock_symbol = selected_stock_symbol.filter(v => {
+                            return v !== 'Basket'
+                        })
                     }
-                    if(custom_data2.length===0){
-                        selected_stock_symbol=selected_stock_symbol.filter(v=>{return v!=='Basket2'})
+                    if (custom_data2.length === 0) {
+                        selected_stock_symbol = selected_stock_symbol.filter(v => {
+                            return v !== 'Basket2'
+                        })
                     }
                     lineChart.updateVis()
                 }))
@@ -311,26 +312,28 @@ class BubbleChart {
             });
     }
 
-    clickBubble (event, d){
+    clickBubble(event, d) {
         {
             const isSelected = (selected_stock_symbol.includes(d.symbol));
             d3.select(this).classed("selected", !isSelected);
-            if(isSelected){
-                selected_stock_symbol=selected_stock_symbol.filter(v=>{return (v!== d.symbol)})
-            }else{
+            if (isSelected) {
+                selected_stock_symbol = selected_stock_symbol.filter(v => {
+                    return (v !== d.symbol)
+                })
+            } else {
                 selected_stock_symbol.push(d.symbol)
             }
             updateLineChart();
         }
     }
 
-    showToolTip(e,d){
-        const formatMarketCap = number => d3.format('.3s')(number).replace('G','Billions').replace('T','Trillions')
+    showToolTip(e, d) {
+        const formatMarketCap = number => d3.format('.3s')(number).replace('G', 'Billions').replace('T', 'Trillions')
         let num = formatMarketCap(d.marketcap);
         d3.select('#tooltip')
             .style("display", "block")
-            .style("top", e.pageY +20+ "px")
-            .style("left", e.pageX + 20+"px")
+            .style("top", e.pageY + 20 + "px")
+            .style("left", e.pageX + 20 + "px")
             .html(`<strong>${d.symbol}</strong>
               <div><strong>${d.name}</strong></div>
               <div><i>Amount of Market Capitalization:</i></div>
@@ -345,8 +348,8 @@ class BubbleChart {
 
     zoomed(e) {
         let vis = this;
-        vis.transform=e.transform.k
-        vis.radiusScale.range([5/e.transform.k,50/e.transform.k]);
+        vis.transform = e.transform.k
+        vis.radiusScale.range([5 / e.transform.k, 50 / e.transform.k]);
         vis.circle.attr("transform", e.transform)
             .attr("r", (d) => vis.radiusScale(d.marketcap));
         vis.XaxisG.call(vis.Xaxis.scale(e.transform.rescaleX(vis.xScale)));
@@ -357,7 +360,7 @@ class BubbleChart {
 
     autoZoomed(e) {
         let vis = this;
-        vis.radiusScale.range([5/e.transform.k,50/e.transform.k]);
+        vis.radiusScale.range([5 / e.transform.k, 50 / e.transform.k]);
         vis.circle
             .attr("transform", e.transform)
             .attr("r", (d) => vis.radiusScale(d.marketcap));
@@ -379,7 +382,7 @@ class BubbleChart {
         d3.selectAll("circle.selected")
             .classed("selected", false)
             .each(function (d) {
-                selected_stock_symbol = selected_stock_symbol.filter(v => v!==d.symbol);
+                selected_stock_symbol = selected_stock_symbol.filter(v => v !== d.symbol);
             });
         updateLineChart();
     }
@@ -387,8 +390,8 @@ class BubbleChart {
     initialZoom() {
         let vis = this;
         // initial zoom
-        let initialTransform = d3.zoomIdentity.translate(vis.innerWidth/2, vis.innerHeight/2)
-            .scale(5).translate(-vis.innerWidth/5,-vis.innerHeight/1.3);
+        let initialTransform = d3.zoomIdentity.translate(vis.innerWidth / 2, vis.innerHeight / 2)
+            .scale(5).translate(-vis.innerWidth / 5, -vis.innerHeight / 1.3);
         vis.svg.call(vis.zoom.transform, initialTransform);
         // go back to normal
         vis.svg
@@ -408,7 +411,7 @@ class BubbleChart {
         vis.svg.select('#bubblechart_title')
             .attr("x", 500)
             .attr("y", 30)
-            .attr('fill','white')
+            .attr('fill', 'white')
             .attr("text-anchor", "middle")
             .attr('font-size', '18px')
             .attr('font-weight', 'bold')
